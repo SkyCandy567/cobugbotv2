fd1f8ccf3abd4f34c6e44aa4252addb3ee30a977
dpbroberg <dpbroberg@localhost>
2020-07-03 00:24:30 -0400

    Use random library for getting search status array index

--- commands/reddit.js
+++ commands/reddit.js
@@ -3,12 +3,13 @@ module.exports = reddit;
 
 const redditEngine = require("../lib/redditEngine");
 const searchStatus = require("../lib/searchStatus");
+const random = require('random');
 
 reddit.execute = async function(client, msg, cmd, args)
 {
     console.log(`Fetching ${cmd}`);
 
-    let randomSearch = Math.floor(Math.random() * Math.floor(searchStatus.collection.length));
+    let randomSearch = random.int(0, searchStatus.collection.length - 1);
     if(msg.deletable) await msg.delete();
 
     let sentMessage = await msg.channel.send(searchStatus.collection[randomSearch]);
